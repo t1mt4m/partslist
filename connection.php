8621f5ba42d2c2e792c5d891faf47601925a3326
t1mt4m <tam.k@oundleschool.org.uk>
2024-06-07 09:02:21 +0100

    whoops wrong place

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "partslist";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    //set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
+    }
+    catch(PDOEception $e)
+    {
+        echo "failed to connect: " . $e->getMessage();
+    }
+?>
