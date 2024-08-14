3199a3e3ab451c60c3e5ebbb641bd5619e39af95
t1mt4m <tam.k@oundleschool.org.uk>
2024-08-14 01:49:18 +0100

    install.php 1

--- install.php
+++ install.php
@@ -1,3 +1,37 @@
 <?php
 include_once("connection.php");
-$stmt=$conn->prepare("DROP TABLE IF EXISTS ")
+#create tables
+$stmt=$conn->prepare("DROP TABLE IF EXISTS cars;
+CREATE TABLE `cars` (
+  `cars` varchar(128) NOT NULL
+);
+CREATE TABLE `parts` (
+  `partID` int(4) NOT NULL,
+  `partName` varchar(128) NOT NULL,
+  `price` float NOT NULL,
+  `partType` smallint(3) NOT NULL
+);
+CREATE TABLE `parttype` (
+  `partTypeNo` smallint(3) NOT NULL,
+  `partType` varchar(64) NOT NULL
+);
+CREATE TABLE `projecthasparts` (
+  `projectID` int(8) NOT NULL,
+  `partID` int(4) NOT NULL,
+  `fitted` tinyint(1) NOT NULL,
+  `inShop` tinyint(1) NOT NULL,
+  `works` tinyint(1) NOT NULL,
+  `notes` varchar(256) NOT NULL
+);
+CREATE TABLE `projects` (
+  `projectID` int(4) NOT NULL,
+  `userID` int(8) NOT NULL,
+  `projectName` varchar(32) NOT NULL,
+  `projectHost` varchar(32) NOT NULL
+);
+CREATE TABLE `users` (
+  `userID` int(8) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL
+);
+")
