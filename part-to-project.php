de76b3be1c8fdcd5558afd96f99faf3f22f76cd6
t1mt4m <tam.k@oundleschool.org.uk>
2024-11-28 11:37:38 +0000

    commit

--- part-to-project.php
+++ part-to-project.php
@@ -21,7 +21,6 @@
     <!--Creates a drop down list-->
     part:<select partID="part">
     <?php
-	    include_once('connection.php');
     	$stmt = $conn->prepare("SELECT * FROM parts ORDER BY partType ASC");
     	$stmt->execute();
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
